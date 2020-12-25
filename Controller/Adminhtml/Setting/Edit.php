42d99b7b8b2836e8e14d93f38db9bbd912dc592f
zuongthao <zuongthao@gmail.com>
2020-12-25 14:52:58 +0700

    add Blog Theme Puremedia Code

---
+++ Controller/Adminhtml/Setting/Edit.php
@@ -0,0 +1,12 @@
+<?php
+namespace ZT\BlogThemePuremedia\Controller\Adminhtml\Setting;
+
+use ZT\BlogTheme\Controller\Adminhtml\Setting\Edit as ThemeEdit;
+
+/**
+ * Edit PWA Blog Theme action.
+ */
+class Edit extends ThemeEdit
+{
+
+}
